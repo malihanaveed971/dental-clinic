cfdc6dd93001089fc88674f77d84b8ba4ed7abf0
malihanaveed971 <malihanaveed75@gmail.com>
2026-07-04 12:42:51 +0500

    fix:change dentist table

--- database/migrations/2026_06_06_143017_create_dentists_table.php
+++ database/migrations/2026_06_06_143017_create_dentists_table.php
@@ -1,8 +1,10 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDentistsTable extends Migration
 {
     public function up(): void
     {
@@ -22,4 +24,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('dentists');
     }
-};
+}
